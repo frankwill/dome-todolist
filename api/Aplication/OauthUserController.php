c1a9cdf3e902d098f52e5884238b1f453dd05e7e
Frank William <frank.will999@gmail.com>
2024-10-27 23:42:03 -0300

    style: Respostas HTTP no controlador OAuth

--- api/Aplication/OauthUserController.php
+++ api/Aplication/OauthUserController.php
@@ -12,8 +12,20 @@ class OauthUserController
     $request = file_get_contents("php://input");
     $response = json_decode($request, true);
 
+    if (empty($response['username_user'])) {
+      http_response_code(400);
+      echo json_encode(['message' => 'Parâmetros não enviados']);
+      return;
+    }
+
     $user = $userRepository->find($response['username_user']);
 
-    echo json_encode($user);
+    if ($user === false) {
+      http_response_code(401);
+      echo json_encode(['message' => 'Usuário ou senha inválidas']);
+    } else {
+      header('Content-Type: application/json');
+      echo json_encode($user);
+    }
   }
 }
